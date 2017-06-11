63108730829100f706f39281edee0592f7802b34
Alejandro Baltodano <carlosreyesb99@gmail.com>
2017-06-11 14:24:01 -0600

    Arreglos en la vista subirArchivos

--- resources/views/Archivos/subirArchivo.blade.php
+++ resources/views/Archivos/subirArchivo.blade.php
@@ -6,20 +6,15 @@
                 <div class="panel-body">
 
 					<form>
-					  <div class="form-group">
-					    <label for="titulo">Titulo</label>
-					    <input type="text" class="form-control" name="titulo" placeholder="titulo">
-					  </div>
-
-					  <div class="form-group">
-					    <label for="descripcion">descripcion</label>
-					    <textarea class="form-control" name="descripcion" rows="3"></textarea>
-					  </div>
 
 					  <div class="form-group">
 					    <label for="ubicacionArchivo">File input</label>
 					    <input type="file" class="form-control-file" name="ubicacionArchivo" aria-describedby="fileHelp">
 					    <small id="fileHelp" class="form-text text-muted">This is some placeholder block-level help text for the above input. It's a bit lighter and easily wraps to a new line.</small>
+					  </div>
+					    <div class="form-group">
+					    <label for="comentario">Comentario</label>
+					    <textarea class="form-control" name="comentario" rows="3"></textarea>
 					  </div>
 
 					  <button type="submit" class="btn btn-primary">Submit</button>
